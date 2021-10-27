35805dd48ab6878a9d8ec696080c02e744f48066
Phuong Phung <pkp013@gmail.com>
2021-10-28 06:51:00 +0700

    pp

--- MinerMan/mainwindow.cpp
+++ MinerMan/mainwindow.cpp
@@ -2,7 +2,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QDateTime>
 #include <QProcess>
+int stage = 0;
+int stabCount = 0;
 QHash<QString,QString> miner_output;
 QProcess process_miner;
 QProcess process_oc;
@@ -16,17 +19,23 @@ MainWindow::MainWindow(QWidget *parent)
 
     runProcess();
     startTimer(1000);
+    stage = 0;
 
 }
 // slot connected to a QAction
 void MainWindow::timerEvent(QTimerEvent *event)
 {
-
+    stabCount++;
+    if(stabCount>12&&stage==1)
+    {
+        stage = 2;
+    }
     if(!process_oc.isOpen())
     {
         process_oc.setArguments(QStringList());
         process_oc.start();
     }
+
 //    if(!process_watcher.isOpen())
 //    {
 //        process_watcher.setArguments(QStringList());
@@ -42,9 +51,16 @@ void MainWindow::StartMiner()
 {
     //miner process
     QString sh = "PhoenixMiner/PhoenixMiner.exe" ;
+    process_miner.setProgram(sh);
     QString args = " -pool " +ui->lineEdit_pool1->text()+" -pool2 "+ ui->lineEdit_pool2->text();//asia2.ethermine.org:5555 -wal 0x8f3ef62b3ce472ffe7999dc52701a6957efc6e45.g5phuong";
     args.append(" -wal "+ ui->lineEdit_wallet->text());
-    args.append(" "+ui->plainTextEdit_params->)
+    args.append(" "+ui->plainTextEdit_params->toPlainText());
+    process_miner.setArguments(args.split(" "));
+    if(!process_miner.isOpen())
+    {
+        system("taskkill /im phoenixminer.exe");
+        process_miner.start();
+    }
 }
 void MainWindow::runProcess()
 {
@@ -82,6 +98,20 @@ void MainWindow::readOutputMiner()
         miner_output.insert(key,data);
         updateInfo();
     }
+    if(stage==2)
+    {
+        //check pool
+        if(output.contains("New job"))
+        {
+            if(!output.contains(ui->lineEdit_pool1->text()))
+            if(!output.contains(ui->lineEdit_pool2->text()))
+            {
+                ui->plainTextEdit_system->appendPlainText(QDateTime::currentDateTime().toString("hh:mm:ss")+"pool changed, restart miner\n");
+                on_pushButton_clicked();
+            }
+        }
+
+    }
 
 }
 void MainWindow::readOutputWatcher()
@@ -109,11 +139,11 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-    if(!process_miner.isOpen())
-    {
+
         system("taskkill /im phoenixminer.exe");
         process_miner.start();
-    }
+        stage=1;
+
 
 //    process.write("dir"); // first test
 }
